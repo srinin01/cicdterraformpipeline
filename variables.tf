275eee6398df5a35945ee247e3dee7df1f1b0841
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-11-14 17:03:26 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,5 +30,5 @@ variable "static_web_bucket_name" {
 
 variable "artifacts_bucket_name" {
   description = "S3 Bucket for storing artifacts"
-  default     = "artifacts-bucket"
+  default     = "artifacts-bucket-srinin01"
 }
