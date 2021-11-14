0e4af52f8f0d8c1ef6ec2aa2debe99c5facede3b
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-11-14 17:52:41 -0500

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -6,7 +6,7 @@ resource "aws_codepipeline" "static_web_pipeline" {
   }
 
   artifact_store {
-    location = {"aws_s3_bucket.artifacts_bucket.bucket"}
+    location = aws_s3_bucket.artifacts_bucket.bucket
     type     = "S3"
   }
 
