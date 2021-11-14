00a49f99ecc2b7b3d0f1cc48b8f4f3d810f2b14e
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-11-14 14:24:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "env" {
+  description = "Depolyment environment"
+  default     = "dev"
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "env" {
+  description = "Depolyment environment"
+  default     = "dev"
+}
+
+variable "repository_branch" {
+  description = "Repository branch to connect to"
+  default     = "develop"
+}
+
+variable "repository_owner" {
+  description = "GitHub repository owner"
+  default     = "stojce"
+}
+
+variable "repository_name" {
+  description = "GitHub repository name"
+  default     = "static-web-example"
+}
+
+variable "static_web_bucket_name" {
+  description = "S3 Bucket to deploy to"
+  default     = "static-web-example-bucket"
+}
+
+variable "artifacts_bucket_name" {
+  description = "S3 Bucket for storing artifacts"
+  default     = "artifacts-bucket"
+}
