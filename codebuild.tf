69e0563dc7cf247b7dae5240181126adda26687b
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-11-14 15:10:55 -0500

    Update codebuild.tf

--- codebuild.tf
+++ codebuild.tf
@@ -10,7 +10,7 @@ resource "aws_codebuild_project" "static_web_build" {
   build_timeout  = 60
   name           = "static-web-build"
   queued_timeout = 480
-  service_role   = data.aws_iam_role.build_role.arn
+  service_role   = aws_iam_role.build_role.arn
   tags = {
     Environment = var.env
   }
